94b8319a934bcee9be05dfa6384e677b85322203
Your Name <daquan.jordan22@gmail.com>
2022-06-21 10:43:15 -0400

    heroku deployment

--- server.js
+++ server.js
@@ -16,10 +16,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useFindAndModify: true
-});
+
 
 // routes
 app.use(require("./routes/api.js"));
